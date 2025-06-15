65503b188a7357e0ac9576f1816e3b876497f718
Chorflas2 <123245405+Chorflas2@users.noreply.github.com>
2025-06-15 20:44:54 +0400

    Delete TestolHub.lua

--- TestolHub.lua
+++
@@ -1,6 +0,0 @@
-game.StarterGui:SetCore("SendNotification", {
-    Title = "Testol Hub";
-    Text = "Link By Chorfla";
-    Duration = 30; -- Duration in seconds
-})
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hoodratleahh/Grow-a-Garden-/refs/heads/main/beeupdategag.lua"))()
