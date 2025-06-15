12f65629e46d69f17c2f3aa96c6a76761a2456d7
Chorflas2 <123245405+Chorflas2@users.noreply.github.com>
2025-06-15 20:41:35 +0400

    Create TestolHub.lua

---
+++ TestolHub.lua
@@ -0,0 +1,6 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Testol Hub";
+    Text = "Link By Chorfla";
+    Duration = 30; -- Duration in seconds
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoodratleahh/Grow-a-Garden-/refs/heads/main/beeupdategag.lua"))()
